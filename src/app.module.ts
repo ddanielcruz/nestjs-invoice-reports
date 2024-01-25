b52957469c3ddb2fa2f2c834f263fbf5e0466fe2
Daniel Cruz <danielcunha54@gmail.com>
2024-01-25 20:09:30 -0300

    feat: create http logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common'
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common'
 
 import { DatabaseModule } from './database/database.module'
 import { ConfigModule } from './shared/config/config.module'
+import { LoggerMiddleware } from './shared/middleware/logger.middleware'
 import { UserModule } from './user/user.module'
 
 @Module({
   imports: [ConfigModule, DatabaseModule, UserModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL })
+  }
+}
---
+++ src/shared/middleware/logger.middleware.ts
@@ -0,0 +1,22 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common'
+import { NextFunction, Request, Response } from 'express'
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP')
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { ip, method, originalUrl } = request
+    const start = Date.now()
+
+    response.on('finish', () => {
+      const { statusCode } = response
+      const duration = Date.now() - start
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} ${duration}ms - ${ip}`,
+      )
+    })
+
+    next()
+  }
+}
